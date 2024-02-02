860b87497319d29122fc363470ee9e95ad357625
yujinpan <yujinpan@users.noreply.github.com>
2024-02-02 10:40:57 +0000

    deploy: f79d7f728d17e7165e92ca1936c688bd32c70704

---
+++ assets/chunks/function1.xNzjZyMG.js
@@ -0,0 +1,1 @@
+import{_ as t,c as n,o}from"./framework.BzEVYYPP.js";const c={data(){return{}}},e={class:"div"};function s(r,a,_,i,d,f){return o(),n("div",e," function1 content ")}const u=t(c,[["render",s]]);export{u as default};
